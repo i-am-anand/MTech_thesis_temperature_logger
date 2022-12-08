3cabec616805dbe0483d8df313c78910cdf63544
Anand <i-am-anand@users.noreply.github.com>
2022-12-08 23:28:37 +0530

    Extended 6 to sensors
    Code optimization and additional sensors inclusion

--- actualSampleTesting.ino
+++ actualSampleTesting.ino
@@ -19,15 +19,12 @@ NEED to NOTE the time when pin 2 is connected to VCC for timing log-------------
 */
 
 #include <EEPROM.h>
-const int sensorAmbient = A3;  // Analog input pin that the ambient LM35 temp sensor is attached to
-const int sensorSample1= A4; 
-const int sensorSample2= A5; //  Analog input pin that the sample's LM35 temp sensor is attached to
+const int sensorAmbient = A0;  // Analog input pin that the ambient LM35 temp sensor is attached to
+const int sensorSample1= A1,sensorSample2= A2,sensorSample3= A3,sensorSample4= A4,sensorSample5= A5; 
 
 int loggingActive=0;
 
-int sensorValueAmbient = 0;        // ADC value read from the sensor
-int sensorValueSample1 = 0;        // ADC value read from the sensor
-int sensorValueSample2 = 0;        
+int sensorValueSample = 0;        // ADC value read from the sensor       
 
 float analogVoltage=0;
 float temperature=0;
@@ -45,15 +42,10 @@ void setup() {
   pinMode(8,OUTPUT); //only for connections
   digitalWrite(8,HIGH);
 
-// pinMode(A1,INPUT);
-    pinMode(A3,INPUT);
-  pinMode(A4,INPUT);
-  pinMode(A5,INPUT);
-  
-  
-   analogReference(INTERNAL);                      // -------------------------------MAJOR
+  pinMode(sensorAmbient,INPUT);
+  pinMode(sensorSample1,INPUT); pinMode(sensorSample2,INPUT); pinMode(sensorSample3,INPUT);pinMode(sensorSample4,INPUT); pinMode(sensorSample5,INPUT);  
 
-
+        analogReference(INTERNAL);                      // -------------------------------MAJOR
 }
 
 void loop() {
@@ -68,20 +60,35 @@ loggingActive=digitalRead(2);
     else
       eepromAddr=1;  */
   // read the ambient analog in value:
-  sensorValueAmbient = analogRead(sensorAmbient);
-  EEPROM.put(eepromAddr,sensorValueAmbient);
-  eepromAddr++;
-  eepromAddr++;
-  sensorValueSample1 = analogRead(sensorSample1);
+            sensorValueSample = analogRead(sensorAmbient);
   EEPROM.put(eepromAddr,sensorValueSample1);
+  eepromAddr++;
+  eepromAddr++;
+            sensorValueSample = analogRead(sensorSample1);
+  EEPROM.put(eepromAddr,sensorValueSample);
   EEPROM.put(0,eepromAddr);                             //-----------CHANGE AS IT CAN HOLD UPTO 255 ONLY
   eepromAddr++; 
   eepromAddr++;
-  sensorValueSample2 = analogRead(sensorSample2);
-/*  EEPROM.put(eepromAddr,sensorValueSample2);
+            sensorValueSample = analogRead(sensorSample2);
+  EEPROM.put(eepromAddr,sensorValueSample);
   EEPROM.put(0,eepromAddr);     //EEPROM addr 0 stores last eeprom address written. Divide by 4 to see no. of logs.
   eepromAddr++; 
-  eepromAddr++; */
+  eepromAddr++; 
+           sensorValueSample = analogRead(sensorSample3);
+  EEPROM.put(eepromAddr,sensorValueSample);
+  EEPROM.put(0,eepromAddr);                             //-----------CHANGE AS IT CAN HOLD UPTO 255 ONLY
+  eepromAddr++; 
+  eepromAddr++;
+           sensorValueSample = analogRead(sensorSample4);
+  EEPROM.put(eepromAddr,sensorValueSample);
+  EEPROM.put(0,eepromAddr);                             //-----------CHANGE AS IT CAN HOLD UPTO 255 ONLY
+  eepromAddr++; 
+  eepromAddr++;
+           sensorValueSample = analogRead(sensorSample5);
+  EEPROM.put(eepromAddr,sensorValueSample);
+  EEPROM.put(0,eepromAddr);                             //-----------CHANGE AS IT CAN HOLD UPTO 255 ONLY
+  eepromAddr++; 
+  eepromAddr++;
   //send over radio
   delay(180000); // delay(1799998); //15 mins delay including entire loop delay COMPENSATE EXTRA DELAY AT the END   //make dealy 1799998
   }
@@ -97,38 +104,17 @@ loggingActive=digitalRead(2);
       addr=2;
       while(addr<=temp){                   
       
-          EEPROM.get(addr,sensorValueAmbient);  
-          analogVoltage = (float)sensorValueAmbient*1.1/1024;       //set value according to AREF---------------------------------
+          EEPROM.get(addr,sensorValueSample);  
+          analogVoltage = (float)sensorValueSample*1.1/1024;       //set value according to AREF---------------------------------
           temperature=100*analogVoltage; 
          // Serial.print(" ");
-          Serial.print(sensorValueAmbient);
+          Serial.print(sensorValueSample);
           Serial.print("\t ");
           Serial.print(analogVoltage,1);
           Serial.print("\t ");
           Serial.println(temperature,1);
           addr += sizeof(int);
-          
-          EEPROM.get(addr,sensorValueSample1);  
-          analogVoltage = (float)sensorValueSample1*1.1/1024;       //set value according to AREF---------------------------------
-          temperature=100*analogVoltage; 
-      //    Serial.print("analogValue = ");
-          Serial.print(sensorValueSample1);
-          Serial.print("\t ");
-          Serial.print(analogVoltage,1);
-          Serial.print("\t ");
-          Serial.println(temperature,1);
-          addr += sizeof(int);
-
-/*          EEPROM.get(addr,sensorValueSample2);  
-          analogVoltage = (float)sensorValueSample2*1.1/1024;       //set value according to AREF---------------------------------
-          temperature=100*analogVoltage; 
-        //  Serial.print("analogValue = ");
-          Serial.print(sensorValueSample2);
-          Serial.print("\t ");
-          Serial.print(analogVoltage,1);
-          Serial.print("\t ");
-          Serial.println(temperature,1);
-          addr += sizeof(int);      */   
+       
         }
       }
     }
