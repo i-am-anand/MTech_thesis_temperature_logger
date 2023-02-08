f9f7724cdd511b0954e9d69f7c09b5fca2bf6969
Anand <i-am-anand@users.noreply.github.com>
2023-02-08 12:54:37 +0530

    5 sensors usb serial test code

---
+++ ds18b20_5sensorsTest.ino
@@ -0,0 +1,60 @@
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+#define sensorCount 5 //no of sensors on a single onewire
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+/*
+ * The setup function. We only start the sensors here
+ */
+void setup(void)
+{
+  // start serial port
+//  digitalWrite(7,HIGH); //------------------------------------------------------
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
+}
+
+/*
+ * Main function, get and show the temperature
+ */
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  delay(10);
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+ for(int i=0;i<sensorCount;i++)
+  {
+  float tempC = sensors.getTempCByIndex(i);
+
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+  } 
+  else
+  {
+    Serial.print("Error: Could not read temperature data for:");
+    Serial.println(i+1);
+  }
+  }
+
+delay(4000);
+}
